c325c1939d97c9358451edccdfe15c85fa83a431
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-01-28 08:14:17 +0100

    Reverted logging changes

--- cmd/root.go
+++ cmd/root.go
@@ -13,20 +13,29 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var hasInitLogging bool
 var loglevel string
 var Kubeconfig *rest.Config
 var kubeconfigPath string
 var Namespace string
 
 var rootCmd = &cobra.Command{
-	SilenceErrors: true,
-	SilenceUsage:  true,
-	Use:           "wharf-cmd",
-	Short:         "Ci application to generate .wharf-ci.yml files and execute them against a kubernetes cluster",
-	Long:          ``,
+	Use:   "wharf-ci",
+	Short: "Ci application to generate .wharf-ci.yml files and execute them against a kubernetes cluster",
+	Long:  ``,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		var err error
+
+		parsedLogLevel, err := parseLevel(loglevel)
+		if err != nil {
+			parsedLogLevel = logger.LevelInfo
+		}
+		logger.AddOutput(parsedLogLevel, consolepretty.Default)
+
+		if err != nil {
+			log.Warn().WithStringer("loglevel", parsedLogLevel).Message("Unable to parse loglevel.")
+		} else {
+			log.Debug().WithStringer("loglevel", parsedLogLevel).Message("Setting log-level.")
+		}
+
 		Kubeconfig, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 		if err != nil {
 			log.Warn().WithError(err).Message("Failed to load kube-config")
@@ -38,41 +47,15 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		if !hasInitLogging {
-			initLogging()
-		}
-		log.Error().Message(err.Error())
-		os.Exit(1)
+		log.Panic().WithError(err).Message("Execution failed.")
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initLogging)
 	home := homeDir()
 	rootCmd.PersistentFlags().StringVar(&loglevel, "loglevel", "info", "Show debug information")
 	rootCmd.PersistentFlags().StringVar(&kubeconfigPath, "kubeconfig", filepath.Join(home, ".kube", "config"), "Path to kubeconfig file")
 	rootCmd.PersistentFlags().StringVar(&Namespace, "namespace", "default", "Namespace to spawn resources in")
-}
-
-func initLogging() {
-	parsedLogLevel, err := parseLevel(loglevel)
-	if err != nil {
-		parsedLogLevel = logger.LevelInfo
-	}
-	logConfig := consolepretty.DefaultConfig
-	if parsedLogLevel != logger.LevelDebug {
-		logConfig.DisableCaller = true
-		logConfig.DisableDate = true
-		logConfig.ScopeMinLengthAuto = false
-	}
-	logger.AddOutput(parsedLogLevel, consolepretty.New(logConfig))
-	if err != nil {
-		log.Warn().WithStringer("loglevel", parsedLogLevel).Message("Unable to parse loglevel. Defaulting to 'INFO'.")
-		parsedLogLevel = logger.LevelInfo
-	} else {
-		log.Debug().WithStringer("loglevel", parsedLogLevel).Message("Setting log-level.")
-	}
-	hasInitLogging = true
 }
 
 func homeDir() string {
