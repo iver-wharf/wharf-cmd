f94a503405df5ba078b00fe9e432ef9d39c0763a
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-01-28 09:15:11 +0100

    Reverted pkg/run/run.go changes

--- pkg/run/run.go
+++ pkg/run/run.go
@@ -7,6 +7,7 @@ import (
 	"github.com/iver-wharf/wharf-cmd/pkg/core/containercreator"
 	"github.com/iver-wharf/wharf-cmd/pkg/core/containercreator/git"
 
+	"github.com/iver-wharf/wharf-api-client-go/pkg/wharfapi"
 	"github.com/iver-wharf/wharf-cmd/pkg/core/buildclient"
 	"github.com/iver-wharf/wharf-cmd/pkg/core/kubernetes"
 	"github.com/iver-wharf/wharf-cmd/pkg/core/wharfyml"
@@ -20,7 +21,6 @@ type Runner struct {
 	Kubeconfig  *rest.Config
 	buildClient buildclient.Client
 	AuthHeader  string
-	DryRun      bool
 }
 
 func NewRunner(kubeconfig *rest.Config, authHeader string) Runner {
@@ -51,6 +51,9 @@ func (r Runner) Run(
 
 	def, err := wharfyml.Parse(path, builtinVars)
 	if err != nil {
+		log.Error().WithFunc(withRunMeta).
+			WithError(err).
+			Message("Failed to parse wharf-ci file.")
 		return fmt.Errorf("run: parse definition: %w", err)
 	}
 
@@ -79,13 +82,12 @@ func (r Runner) RunDefinition(
 		return fmt.Errorf("stage %q not found in definition", stageName)
 	}
 
-	// TODO: Remove
-	//err = r.buildClient.PostLogWithStatus(uint(buildID), "run definition", wharfapi.BuildScheduling)
-	//if err != nil {
-	//	log.Error().WithFunc(withRunMeta).
-	//		WithError(err).
-	//		Message("Unable to update build status.")
-	//}
+	err = r.buildClient.PostLogWithStatus(uint(buildID), "run definition", wharfapi.BuildScheduling)
+	if err != nil {
+		log.Error().WithFunc(withRunMeta).
+			WithError(err).
+			Message("Unable to update build status.")
+	}
 
 	podClient, err := kubernetes.NewPodClient(namespace, r.Kubeconfig, gitParams, builtinVars)
 	if err != nil {
@@ -97,52 +99,34 @@ func (r Runner) RunDefinition(
 
 	wg := sync.WaitGroup{}
 	for _, step := range stage.Steps {
-		var podName string
-		if r.DryRun {
-			podName = fmt.Sprintf("wharf-build-%s-%s", stage.Name, step.Name)
-			log.Info().WithFunc(withRunMeta).
-				WithString("pod", podName).
-				Message("Created pod (dry run)")
-		} else {
-			pod, err := podClient.CreatePod(step, stage, buildID)
+		pod, err := podClient.CreatePod(step, stage, buildID)
+		if err != nil {
+			log.Error().WithFunc(withRunMeta).
+				WithError(err).
+				Message("Failed to create pod.")
+
+			err = r.buildClient.PostLogWithStatus(uint(buildID), "Unable to create pod.", wharfapi.BuildFailed)
 			if err != nil {
 				log.Error().WithFunc(withRunMeta).
 					WithError(err).
-					Message("Failed to create pod.")
-
-				// TODO: Remove
-				//err = r.buildClient.PostLogWithStatus(uint(buildID), "Unable to create pod.", wharfapi.BuildFailed)
-				//if err != nil {
-				//	log.Error().WithFunc(withRunMeta).
-				//		WithError(err).
-				//		Message("Unable to update build status.")
-				//}
-				return fmt.Errorf("run definition: create pod: %w", err)
+					Message("Unable to update build status.")
 			}
-			podName = pod.Name
+			return fmt.Errorf("run definition: create pod: %w", err)
 		}
 
-		// TODO: Remove
-		//err = r.buildClient.PostLogWithStatus(uint(buildID), fmt.Sprintf("Pod %s created.", pod.Name), wharfapi.BuildRunning)
-		//if err != nil {
-		//	log.Error().WithFunc(withRunMeta).
-		//		WithError(err).
-		//		Message("Unable to update build status.")
-		//}
+		err = r.buildClient.PostLogWithStatus(uint(buildID), fmt.Sprintf("Pod %s created.", pod.Name), wharfapi.BuildRunning)
+		if err != nil {
+			log.Error().WithFunc(withRunMeta).
+				WithError(err).
+				Message("Unable to update build status.")
+		}
 
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
 			logChannel := make(chan string)
-			if r.DryRun {
-				log.Info().WithFunc(withRunMeta).
-					WithString("pod", podName).
-					Message("Read logs from pod (dry run)")
-				close(logChannel)
-			} else {
-				go podClient.ReadLogsFromPod(logChannel, podName)
-			}
+			go podClient.ReadLogsFromPod(logChannel, pod.Name)
 
 			for message := range logChannel {
 				err := r.buildClient.PostLog(uint(buildID), message)
@@ -153,50 +137,36 @@ func (r Runner) RunDefinition(
 				}
 			}
 
-			if r.DryRun {
-				log.Info().WithFunc(withRunMeta).
-					WithString("pod", podName).
-					Message("Wait until pod has finished (dry run)")
-			} else {
-				done, err := podClient.WaitUntilPodFinished(podName)
-				if err != nil || !done {
-					return
-				}
+			done, err := podClient.WaitUntilPodFinished(pod.Name)
+			if err != nil || !done {
+				return
 			}
 
-			if r.DryRun {
-				log.Info().WithFunc(withRunMeta).
-					WithString("pod", podName).
-					Message("Delete pod (dry run)")
-			} else {
-				err = podClient.DeletePod(podName)
-				if err != nil {
-					log.Error().WithFunc(withRunMeta).
-						WithError(err).
-						WithString("pod", podName).
-						Message("Failed to delete pod.")
-				}
+			err = podClient.DeletePod(pod.Name)
+			if err != nil {
+				log.Error().WithFunc(withRunMeta).
+					WithError(err).
+					WithString("pod", pod.Name).
+					Message("Failed to delete pod.")
 			}
 
-			// TODO: Remove
-			//err = r.buildClient.PostLog(uint(buildID), fmt.Sprintf("Pod %s is deleted.", pod.Name))
-			//if err != nil {
-			//	log.Error().WithFunc(withRunMeta).
-			//		WithError(err).
-			//		Message("Failed to post log.")
-			//}
+			err = r.buildClient.PostLog(uint(buildID), fmt.Sprintf("Pod %s is deleted.", pod.Name))
+			if err != nil {
+				log.Error().WithFunc(withRunMeta).
+					WithError(err).
+					Message("Failed to post log.")
+			}
 		}()
 	}
 
 	wg.Wait()
-	// TODO: Remove
-	//err = r.buildClient.PostLogWithStatus(uint(buildID), "Build completed", wharfapi.BuildCompleted)
-	//if err != nil {
-	//	log.Error().WithFunc(withRunMeta).
-	//		WithError(err).
-	//		WithStringer("newStatus", wharfapi.BuildCompleted).
-	//		Message("Failed to update build status.")
-	//}
+	err = r.buildClient.PostLogWithStatus(uint(buildID), "Build completed", wharfapi.BuildCompleted)
+	if err != nil {
+		log.Error().WithFunc(withRunMeta).
+			WithError(err).
+			WithStringer("newStatus", wharfapi.BuildCompleted).
+			Message("Failed to update build status.")
+	}
 
 	return nil
 }
