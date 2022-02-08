ded010003bb14ceaa8b772ec509e761dcdcafc99
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-02-08 09:35:20 +0100

    Improved test error logging

--- pkg/wharfyml/error_test.go
+++ pkg/wharfyml/error_test.go
@@ -2,6 +2,8 @@ package wharfyml
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -11,16 +13,24 @@ func requireContainsErr(t *testing.T, errs errorSlice, err error) {
 			return
 		}
 	}
-	t.Fatalf("\nexpected contains error: %q\nactual: (len=%d) %v",
-		err, len(errs), errs)
+	t.Fatalf("\nexpected contains error: %q\nactual: (len=%d)\n%s",
+		err, len(errs), formatErrorSlice("  - ", errs))
 }
 
 func requireNotContainsErr(t *testing.T, errs errorSlice, err error) {
 	for i, e := range errs {
 		if errors.Is(e, err) {
-			t.Fatalf("\nexpected not to contain error: %q\nfound at index=%d\nactual: (len=%d) %v",
-				err, i, len(errs), errs)
+			t.Fatalf("\nexpected not to contain error: %q\nfound at index=%d\nactual: (len=%d)\n%s",
+				err, i, len(errs), formatErrorSlice("  - ", errs))
 			return
 		}
 	}
+}
+
+func formatErrorSlice(prefix string, errs errorSlice) string {
+	var sb strings.Builder
+	for i, err := range errs {
+		fmt.Fprintf(&sb, "%s[i=%d] %s\n", prefix, i, err)
+	}
+	return sb.String()
 }
