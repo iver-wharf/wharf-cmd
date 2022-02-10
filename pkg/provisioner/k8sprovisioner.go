9730fe262822cab2ec4328eea2cf1fb18f57b566
Alexamakans <fougner.alexander@gmail.com>
2022-02-10 09:00:18 +0100

    Cleaned up k8sprovisioner.go

--- pkg/provisioner/k8sprovisioner.go
+++ pkg/provisioner/k8sprovisioner.go
@@ -6,7 +6,6 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
@@ -29,7 +28,7 @@ type k8sProvisioner struct {
 	events     corev1.EventInterface
 }
 
-// NewK8sProvisioner returns a new provisioner implementation that targets
+// NewK8sProvisioner returns a new Provisioner implementation that targets
 // Kubernetes using a specific Kubernetes namespace and REST config.
 func NewK8sProvisioner(namespace string, restConfig *rest.Config) (Provisioner, error) {
 	clientset, err := kubernetes.NewForConfig(restConfig)
@@ -66,23 +65,6 @@ func (p k8sProvisioner) DeleteWorker(ctx context.Context, workerID string) error
 func (p k8sProvisioner) CreateWorker(ctx context.Context) (*v1.Pod, error) {
 	podMeta := createPodMeta()
 	newPod, err := p.Pods.Create(ctx, &podMeta, metav1.CreateOptions{})
-	if err != nil {
-		return newPod, err
-	}
-
-	err = p.waitForInitContainerDone(ctx, newPod.ObjectMeta)
-	if err != nil {
-		return newPod, err
-	}
-
-	log.Debug().Message("Init Container done.")
-
-	err = p.waitForAppContainerRunning(ctx, newPod.ObjectMeta)
-	if err != nil {
-		return newPod, err
-	}
-
-	log.Debug().Message("App Container running.")
 	return newPod, err
 }
 
@@ -100,55 +82,76 @@ func (p k8sProvisioner) getWorker(ctx context.Context, workerID string) (*v1.Pod
 }
 
 func createPodMeta() v1.Pod {
+	const (
+		gitCloneContainerName = "init"
+		gitCloneImage         = "bitnami/git:2-debian-10"
+		gitURL                = "http://github.com/iver-wharf/wharf-cmd"
+		repoVolumeName        = "repo"
+		repoVolumeMountPath   = "/mnt/repo"
+
+		workerContainerName = "app"
+		workerImage         = "ubuntu:20.04"
+
+		labelWorkerInstance  = "prod"
+		labelWorkerBuildRef  = "123"
+		labelWorkerProjectID = "456"
+	)
+	var extraLabels = make(map[string]string)
+
+	var labels = map[string]string{
+		"app":                          "wharf-cmd-worker",
+		"app.kubernetes.io/name":       "wharf-cmd-worker",
+		"app.kubernetes.io/part-of":    "wharf",
+		"app.kubernetes.io/managed-by": "wharf-cmd-provisioner",
+		"app.kubernetes.io/created-by": "wharf-cmd-provisioner",
+		"wharf.iver.com/instance":      labelWorkerInstance,
+		"wharf.iver.com/build-ref":     labelWorkerBuildRef,
+		"wharf.iver.com/project-id":    labelWorkerProjectID,
+	}
+	for k, v := range extraLabels {
+		labels[k] = v
+	}
+
 	return v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: "wharf-provisioner",
-			Labels: map[string]string{
-				"app":                          "wharf-cmd-worker",
-				"app.kubernetes.io/name":       "wharf-cmd-worker",
-				"app.kubernetes.io/part-of":    "wharf",
-				"app.kubernetes.io/managed-by": "wharf-cmd-provisioner",
-				"app.kubernetes.io/created-by": "wharf-cmd-provisioner",
-				"wharf.iver.com/instance":      "prod",
-				"wharf.iver.com/build-ref":     "123",
-				"wharf.iver.com/project-id":    "456",
-			},
+			Labels:       labels,
 		},
 		Spec: v1.PodSpec{
 			AutomountServiceAccountToken: new(bool),
 			RestartPolicy:                v1.RestartPolicyNever,
 			InitContainers: []v1.Container{
 				{
-					Name:            "init",
-					Image:           "bitnami/git:2-debian-10",
+					Name:            gitCloneContainerName,
+					Image:           gitCloneImage,
 					ImagePullPolicy: v1.PullIfNotPresent,
-					Args:            append(podInitCloneArgs, "http://github.com/iver-wharf/wharf-cmd", "/mnt/repo"),
+					Args:            append(podInitCloneArgs, gitURL, repoVolumeMountPath),
 					VolumeMounts: []v1.VolumeMount{
 						{
-							Name:      "repo",
-							MountPath: "/mnt/repo",
+							Name:      repoVolumeName,
+							MountPath: repoVolumeMountPath,
 						},
 					},
 				},
 			},
 			Containers: []v1.Container{
 				{
-					Name:            "app",
-					Image:           "ubuntu:20.04",
+					Name:            workerContainerName,
+					Image:           workerImage,
 					ImagePullPolicy: v1.PullAlways,
 					Command:         podContainerListArgs,
-					WorkingDir:      "/mnt/repo",
+					WorkingDir:      repoVolumeMountPath,
 					VolumeMounts: []v1.VolumeMount{
 						{
-							Name:      "repo",
-							MountPath: "/mnt/repo",
+							Name:      repoVolumeName,
+							MountPath: repoVolumeMountPath,
 						},
 					},
 				},
 			},
 			Volumes: []v1.Volume{
 				{
-					Name: "repo",
+					Name: repoVolumeName,
 					VolumeSource: v1.VolumeSource{
 						EmptyDir: &v1.EmptyDirVolumeSource{},
 					},
@@ -156,67 +159,4 @@ func createPodMeta() v1.Pod {
 			},
 		},
 	}
-}
-
-func (p k8sProvisioner) waitForInitContainerDone(ctx context.Context, podMeta metav1.ObjectMeta) error {
-	return p.waitForPodModifiedFunc(ctx, podMeta, func(p *v1.Pod) (bool, error) {
-		return isInitContainerDone(p)
-	})
-}
-
-func (p k8sProvisioner) waitForAppContainerRunning(ctx context.Context, podMeta metav1.ObjectMeta) error {
-	return p.waitForPodModifiedFunc(ctx, podMeta, func(p *v1.Pod) (bool, error) {
-		return isAppContainerRunning(p)
-	})
-}
-
-func isInitContainerDone(pod *v1.Pod) (bool, error) {
-	for _, c := range pod.Status.InitContainerStatuses {
-		if c.State.Terminated != nil {
-			if c.State.Terminated.ExitCode != 0 {
-				return true, fmt.Errorf("non-zero exit code: %d", c.State.Terminated.ExitCode)
-			}
-			return true, nil
-		}
-	}
-	return len(pod.Status.InitContainerStatuses) == 0, nil
-}
-
-func isAppContainerRunning(pod *v1.Pod) (bool, error) {
-	for _, c := range pod.Status.ContainerStatuses {
-		if c.State.Terminated != nil {
-			if c.State.Terminated.ExitCode != 0 {
-				return false, fmt.Errorf("non-zero exit code: %d", c.State.Terminated.ExitCode)
-			}
-			return true, nil
-		}
-		if c.State.Running != nil {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-func (p k8sProvisioner) waitForPodModifiedFunc(ctx context.Context, podMeta metav1.ObjectMeta, f func(p *v1.Pod) (bool, error)) error {
-	w, err := p.Clientset.CoreV1().Pods(p.Namespace).Watch(ctx, metav1.SingleObject(podMeta))
-	if err != nil {
-		return err
-	}
-
-	defer w.Stop()
-	for ev := range w.ResultChan() {
-		pod := ev.Object.(*v1.Pod)
-		switch ev.Type {
-		case watch.Modified:
-			ok, err := f(pod)
-			if err != nil {
-				return err
-			} else if ok {
-				return nil
-			}
-		case watch.Deleted:
-			return fmt.Errorf("pod was removed: %v", pod.Name)
-		}
-	}
-	return fmt.Errorf("got no more events when watching pod: %v", podMeta.Name)
 }
