550032d43211a96a636a56ada359ac5d73b81e6e
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-03-01 12:53:27 +0100

    Added provisionerclient impl

--- pkg/provisionerclient/provisionerclient.go
+++ pkg/provisionerclient/provisionerclient.go
@@ -1,13 +1,103 @@
 package provisionerclient
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/iver-wharf/wharf-cmd/pkg/provisioner"
+	"github.com/iver-wharf/wharf-core/pkg/problem"
 )
 
-type Client struct{}
+// Client is a HTTP client that talks to wharf-cmd-provisioner.
+type Client struct {
+	// APIURL is the base API URL used. Example value:
+	// 	http://wharf-cmd-provisioner.default.svc.cluster.local
+	APIURL string
+}
 
 func (c Client) ListWorkers() ([]provisioner.Worker, error) {
-	return nil, errors.New("not implemented")
+	u, err := buildURL(c.APIURL, "api", "worker")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := doRequest(http.MethodGet, u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	var workers []provisioner.Worker
+	if err := dec.Decode(&workers); err != nil {
+		return nil, err
+	}
+	return workers, nil
+}
+
+func (c Client) DeleteWorker(workerID string) error {
+	u, err := buildURL(c.APIURL, "api", "worker", workerID)
+	if err != nil {
+		return err
+	}
+	resp, err := doRequest(http.MethodDelete, u)
+	if err != nil {
+		return err
+	}
+	return resp.Body.Close()
+}
+
+func doRequest(method string, u *url.URL) (*http.Response, error) {
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err := parseErrorResponse(resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func parseErrorResponse(resp *http.Response) error {
+	if problem.IsHTTPResponse(resp) {
+		prob, err := problem.ParseHTTPResponse(resp)
+		if err != nil {
+			return err
+		}
+		return prob
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("non-2xx status code: %s", resp.Status)
+	}
+	return nil
+}
+
+func buildURL(base string, paths ...string) (*url.URL, error) {
+	u, err := url.Parse(base)
+	if err != nil {
+		return nil, err
+	}
+	if u.Scheme == "" {
+		return nil, fmt.Errorf("URL is missing scheme: %q", base)
+	}
+	if u.Host == "" {
+		return nil, fmt.Errorf("URL is missing host: %q", base)
+	}
+	var pathBuilder strings.Builder
+	var rawPathBuilder strings.Builder
+	if u.Path != "" {
+		pathBuilder.WriteString(strings.TrimSuffix(u.Path, "/"))
+		rawPathBuilder.WriteString(strings.TrimSuffix(u.EscapedPath(), "/"))
+	}
+	for _, segment := range paths {
+		pathBuilder.WriteByte('/')
+		rawPathBuilder.WriteByte('/')
+		pathBuilder.WriteString(segment)
+		rawPathBuilder.WriteString(url.PathEscape(segment))
+	}
+	u.Path = pathBuilder.String()
+	u.RawPath = rawPathBuilder.String()
+	return u, nil
 }
