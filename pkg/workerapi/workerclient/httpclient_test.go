ad0b6d4b4d71c4e1967b1a9ba368c7dda5299828
Alexamakans <fougner.alexander@gmail.com>
2022-03-02 16:42:21 +0100

    Added test for errorIfBad

---
+++ pkg/workerapi/workerclient/httpclient_test.go
@@ -0,0 +1,75 @@
+package workerclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/iver-wharf/wharf-core/pkg/problem"
+	"github.com/stretchr/testify/assert"
+)
+
+var errNormal = errors.New("normal error")
+var errParse = errors.New("failed to parse \"application/problem+json\" problem response: unexpected end of JSON input")
+var errProblem = problem.Response{
+	Type:   "/prob/something",
+	Detail: "Some details",
+	Status: 500,
+}
+
+func TestErrorIfBad(t *testing.T) {
+	probBytes, err := json.Marshal(&errProblem)
+	assert.Nil(t, err)
+	testCases := []struct {
+		name    string
+		wantErr error
+		resp    *http.Response
+		err     error
+	}{
+		{
+			name:    "no problem response gives error",
+			wantErr: errNormal,
+			resp:    &http.Response{},
+			err:     errNormal,
+		},
+		{
+			name:    "nil gives nil",
+			wantErr: nil,
+			resp:    &http.Response{},
+			err:     nil,
+		},
+		{
+			name:    "malformed problem response gives parse error",
+			wantErr: errProblem,
+			resp: &http.Response{
+				Header: http.Header{
+					"Content-Type": []string{problem.HTTPContentType},
+				},
+				Body: io.NopCloser(bytes.NewReader(probBytes)),
+			},
+			err: errNormal,
+		},
+		{
+			name:    "problem response gives problem response",
+			wantErr: errParse,
+			resp: &http.Response{
+				Header: http.Header{
+					"Content-Type": []string{problem.HTTPContentType},
+				},
+				Body: io.NopCloser(bytes.NewReader([]byte{})),
+			},
+			err: errNormal,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			gotErr := errorIfBad(tc.resp, tc.err)
+			assert.Equal(t, fmt.Sprintf("%v", tc.wantErr), fmt.Sprintf("%v", gotErr))
+		})
+	}
+}
