6e867d787cc393ff6c08f28b15ce21378e4444f6
Alexamakans <fougner.alexander@gmail.com>
2022-03-14 12:08:29 +0100

    Changed to use workerclient and wharf-api client

--- pkg/aggregator/k8saggregator.go
+++ pkg/aggregator/k8saggregator.go
@@ -1,16 +1,17 @@
 package aggregator
 
 import (
-	"bufio"
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 
+	"github.com/iver-wharf/wharf-api-client-go/v2/pkg/model/request"
+	"github.com/iver-wharf/wharf-api-client-go/v2/pkg/wharfapi"
+	"github.com/iver-wharf/wharf-cmd/pkg/workerapi/workerclient"
 	"github.com/iver-wharf/wharf-core/pkg/logger"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -27,7 +28,7 @@ var log = logger.NewScoped("AGGREGATOR")
 
 const (
 	wharfAPIExternalPort  = 5001
-	workerAPIExternalPort = 27017
+	workerAPIExternalPort = 5010
 )
 
 // Copied from pkg/provisioner/k8sprovisioner.go
@@ -62,6 +63,9 @@ func NewK8sAggregator(namespace string, restConfig *rest.Config) (Aggregator, er
 
 		upgrader:   upgrader,
 		httpClient: httpClient,
+		wharfClient: wharfapi.Client{
+			APIURL: "http://localhost:5001",
+		},
 	}, nil
 }
 
@@ -70,34 +74,88 @@ type k8sAggregator struct {
 	Clientset *kubernetes.Clientset
 	Pods      corev1.PodInterface
 
-	restConfig *rest.Config
-	upgrader   spdy.Upgrader
-	httpClient *http.Client
+	restConfig  *rest.Config
+	upgrader    spdy.Upgrader
+	httpClient  *http.Client
+	wharfClient wharfapi.Client
 }
 
 func (a k8sAggregator) Serve() error {
-	log.Info().Message("Serve started")
-	var err error
-	var podList *v1.PodList
-	podList, err = a.listMatchingPods(context.Background())
+	log.Info().Message("Aggregator started")
+	podList, err := a.listMatchingPods(context.Background())
 	if err != nil {
 		return err
 	}
 
-	for _, item := range podList.Items {
-		log.Info().WithString("name", item.Name).Message("")
+	for _, pod := range podList.Items {
+		port, connCloser, err := a.connect(pod.Namespace, pod.Name)
+		if err != nil {
+			return err
+		}
+
+		client, err := workerclient.New(fmt.Sprintf("http://127.0.0.1:%d", port.Local), workerclient.Options{
+			InsecureSkipVerify: true,
+		})
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			outStream, err := a.wharfClient.CreateBuildLogStream(context.Background())
+			stream, err := client.StreamLogs(context.Background(), &workerclient.LogsRequest{})
+			if err != nil {
+				log.Error().WithError(err).Message("Fetching stream failed.")
+				return
+			}
+			for line, err := stream.Recv(); err == nil; line, err = stream.Recv() {
+				outStream.Send(request.Log{
+					BuildID:      uint(line.BuildID),
+					WorkerLogID:  uint(line.LogID),
+					WorkerStepID: uint(line.StepID),
+					Timestamp:    line.GetTimestamp().AsTime(),
+					Message:      line.GetMessage(),
+				})
+			}
+
+			summary, err := outStream.CloseAndRecv()
+			if err != nil {
+				log.Error().WithError(err).Message("Close and Recv failed.")
+			}
+			log.Debug().WithUint("logsInserted", summary.LogsInserted).Message("Sent logs to DB.")
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			stream, err := client.StreamArtifactEvents(context.Background(), &workerclient.ArtifactEventsRequest{})
+			if err != nil {
+				log.Error().WithError(err).Message("Fetching stream failed.")
+				return
+			}
+			for artifactEvent, err := stream.Recv(); err == nil; artifactEvent, err = stream.Recv() {
+				log.Info().WithStringer("artifactEvent", artifactEvent).Message("")
+			}
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			stream, err := client.StreamStatusEvents(context.Background(), &workerclient.StatusEventsRequest{})
+			if err != nil {
+				log.Error().WithError(err).Message("Fetching stream failed.")
+				return
+			}
+			for statusEvent, err := stream.Recv(); err == nil; statusEvent, err = stream.Recv() {
+				log.Info().WithStringer("statusEvent", statusEvent).Message("")
+			}
+		}()
+
+		wg.Wait()
+
+		client.Close()
+		connCloser()
 	}
 
-	body, err := a.do(http.MethodGet, "/api")
-	if err != nil {
-		return err
-	}
-
-	bytes, err := io.ReadAll(body)
-	if err != nil {
-		return err
-	}
-	log.Info().WithString("body", string(bytes)).Message("Serve ended")
+	log.Info().Message("Aggregator ended")
 	return nil
 }
 
@@ -105,80 +163,47 @@ func (a k8sAggregator) listMatchingPods(ctx context.Context) (*v1.PodList, error
 	return a.Pods.List(ctx, listOptionsMatchLabels)
 }
 
-func (a k8sAggregator) do(method, address string) (io.ReadCloser, error) {
+type connectionCloser func()
+
+func (a k8sAggregator) connect(namespace, podName string) (*portforward.ForwardedPort, connectionCloser, error) {
 	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward",
-		"default", "wharf-build-container-test-wharfable-w294b")
-	log.Info().WithString("path", path).Message("starting request")
+		namespace, podName)
 	hostBase := strings.TrimLeft(a.restConfig.Host, "htps:/")
 	hostSplit := strings.Split(hostBase, ":")
 	hostIP := hostSplit[0]
 	hostPort, err := strconv.Atoi(hostSplit[1])
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	url := net.FormatURL("https", hostIP, hostPort, path)
-	log.Info().WithStringer("url", url).Message("starting request")
-
-	dialer := spdy.NewDialer(a.upgrader, a.httpClient, method, url)
+	dialer := spdy.NewDialer(a.upgrader, a.httpClient, http.MethodGet, url)
 	stopCh, readyCh := make(chan struct{}, 1), make(chan struct{}, 1)
 	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
 	forwarder, err := portforward.New(dialer,
-		[]string{fmt.Sprintf("0:%d", 10000)},
+		[]string{fmt.Sprintf("0:%d", workerAPIExternalPort)},
 		stopCh, readyCh, out, errOut)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	go func() {
-		for range readyCh {
+		if err := forwarder.ForwardPorts(); err != nil {
+			log.Error().WithError(err).Message("Error occurred during forwarding.")
 		}
-		ports, err := forwarder.GetPorts()
-		if err == nil {
-			log.Info().WithInt("local", int(ports[0].Local)).WithInt("remote", int(ports[0].Remote)).Message("PORTS")
-
-			resp, err := http.Get(fmt.Sprintf("http://localhost:%d/api", ports[0].Local))
-			if err != nil {
-				log.Error().WithError(err).Message("request error")
-				return
-			}
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				log.Error().WithError(err).Message("request error")
-				return
-			}
-			log.Info().WithString("body", string(body)).Message("success")
-		}
-
-		close(stopCh)
 	}()
-	if err := forwarder.ForwardPorts(); err != nil {
-		log.Info().Message("forward err")
-		return nil, err
+
+	for range readyCh {
+	}
+	closerFunc := func() {
+		close(stopCh)
 	}
 
-	if len(errOut.String()) != 0 {
-		log.Info().Message("err")
-		return nil, errors.New(errOut.String())
-	} else if len(out.String()) != 0 {
-		log.Info().Message("out")
-		log.Info().Message(out.String())
-		return io.NopCloser(bufio.NewReader(out)), nil
+	ports, err := forwarder.GetPorts()
+	if err != nil {
+		closerFunc()
+		return nil, nil, err
 	}
-	log.Info().Message("no response")
-	return nil, errors.New("no response")
-}
 
-type forwardedConnection interface {
-	Do(method, address string) (*http.Response, error)
-}
-
-type connection struct {
-	forwarder *portforward.PortForwarder
-	ports     *portforward.ForwardedPort
-	stopCh    chan struct{}
-}
-
-func (c *connection) Do(method, address string) (*http.Response, error) {
-	return nil, nil
+	return &ports[0], closerFunc, nil
 }
