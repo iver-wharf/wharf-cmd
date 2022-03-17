de762463d6d59e0a4ddff247bfa74db2b5da53aa
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-03-17 09:36:27 +0100

    Changed from `interface{}` to `any`

--- pkg/resultstore/keyedmutex.go
+++ pkg/resultstore/keyedmutex.go
@@ -6,13 +6,13 @@ type keyedMutex struct {
 	m sync.Map
 }
 
-func (k keyedMutex) Lock(key interface{}) {
+func (k keyedMutex) Lock(key any) {
 	val, _ := k.m.LoadOrStore(key, &sync.Mutex{})
 	mutex := val.(*sync.Mutex)
 	mutex.Lock()
 }
 
-func (k keyedMutex) Unlock(key interface{}) {
+func (k keyedMutex) Unlock(key any) {
 	val, ok := k.m.Load(key)
 	if !ok {
 		return
