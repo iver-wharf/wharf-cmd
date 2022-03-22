bba58dbac69878de0dfd36f2eb110130a4252cc8
Alexamakans <fougner.alexander@gmail.com>
2022-03-22 11:32:59 +0100

    Added kill endpoint

--- pkg/workerapi/workerserver/restserver.go
+++ pkg/workerapi/workerserver/restserver.go
@@ -22,13 +22,17 @@ func newRestServer(artifactOpener ArtifactFileOpener) *restServer {
 	}
 }
 
-func serveHTTP(s *restServer, listener net.Listener) error {
+func serveHTTP(workerServer *server, s *restServer, listener net.Listener) error {
 	r := gin.New()
 	applyGinHandlers(r)
 	applyCORSConfig(r)
 
 	g := r.Group("/api")
 	g.GET("/", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+	g.GET("/kill", func(c *gin.Context) {
+		c.Status(200)
+		workerServer.Close()
+	})
 
 	s.registerModules(g)
 	return r.RunListener(listener)
