9d605c9f4675aecdf861a2c36b9d044bf488407e
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-04-12 08:28:43 +0200

    Added ignorer.NewAny

--- internal/ignorer/ignorer.go
+++ internal/ignorer/ignorer.go
@@ -6,3 +6,20 @@ type Ignorer interface {
 	// Ignore returns true to ignore a file, and false to include the file.
 	Ignore(absPath, relPath string) bool
 }
+
+// NewAny returns an Ignorer implementation that returns true if any of the
+// provided ignorers return true.
+func NewAny(ignorers ...Ignorer) Ignorer {
+	return ignoreIfAny(ignorers)
+}
+
+type ignoreIfAny []Ignorer
+
+func (m ignoreIfAny) Ignore(absPath, relPath string) bool {
+	for _, i := range m {
+		if i.Ignore(absPath, relPath) {
+			return true
+		}
+	}
+	return false
+}
