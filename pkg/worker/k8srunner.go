06e8e0663bd70e01f7e814487cefcd3631cd447c
Alexamakans <fougner.alexander@gmail.com>
2022-04-25 15:41:28 +0200

    Assignment in type-switch statement

--- pkg/worker/k8srunner.go
+++ pkg/worker/k8srunner.go
@@ -252,27 +252,25 @@ func (r k8sStepRunner) waitForPodModifiedFunc(ctx context.Context, podMeta metav
 	}
 	defer w.Stop()
 	for ev := range w.ResultChan() {
-		switch ev.Object.(type) {
+		switch obj := ev.Object.(type) {
 		case *v1.Pod:
-			pod := ev.Object.(*v1.Pod)
 			switch ev.Type {
 			case watch.Modified:
-				ok, err := f(pod)
+				ok, err := f(obj)
 				if err != nil {
 					return err
 				} else if ok {
 					return nil
 				}
 			case watch.Deleted:
-				return fmt.Errorf("pod was removed: %v", pod.Name)
+				return fmt.Errorf("pod was removed: %v", obj.Name)
 			}
 		case *metav1.Status:
 			if errors.Is(ctx.Err(), context.Canceled) {
 				return fmt.Errorf("watching pod: %s: %w", podMeta.Name, ctx.Err())
 			}
 
-			status := ev.Object.(*metav1.Status)
-			return fmt.Errorf("error for pod: %v: %v", podMeta.Name, errors.New(status.Message))
+			return fmt.Errorf("error for pod: %v: %v", podMeta.Name, errors.New(obj.Message))
 		}
 	}
 	return fmt.Errorf("got no more events when watching pod: %v", podMeta.Name)
