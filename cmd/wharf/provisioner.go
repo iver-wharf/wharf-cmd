1ad3edb8407e43a49e5f9ab064b6a3089f65a324
Alexamakans <fougner.alexander@gmail.com>
2022-05-05 10:36:10 +0200

    Moved loading of kubeconfig to newProvisioner

--- cmd/wharf/provisioner.go
+++ cmd/wharf/provisioner.go
@@ -3,23 +3,25 @@ package main
 import (
 	"github.com/iver-wharf/wharf-cmd/pkg/provisioner"
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/rest"
 )
 
 var provisionerFlags = struct {
-	restConfig *rest.Config
-
 	instanceID string
 }{
 	instanceID: "local",
 }
 
 func newProvisioner() (provisioner.Provisioner, error) {
+	restConfig, ns, err := loadKubeconfig()
+	if err != nil {
+		return nil, err
+	}
+	rootConfig.K8s.Namespace = ns
 	return provisioner.NewK8sProvisioner(
 		provisionerFlags.instanceID,
 		rootConfig.Provisioner,
 		rootConfig.K8s.Namespace,
-		provisionerFlags.restConfig)
+		restConfig)
 }
 
 var provisionerCmd = &cobra.Command{
@@ -32,19 +34,6 @@ The "wharf provisioner" act as a fire-and-forget, where the entire build
 orchestration is handled inside the Kubernetes cluster, in comparison to the
 "wharf run" command that uses your local machine to orchestrate the build.
 `,
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		if err := callParentPersistentPreRuns(cmd, args); err != nil {
-			return err
-		}
-
-		restConfig, ns, err := loadKubeconfig()
-		if err != nil {
-			return err
-		}
-		provisionerFlags.restConfig = restConfig
-		rootConfig.K8s.Namespace = ns
-		return nil
-	},
 }
 
 func init() {
