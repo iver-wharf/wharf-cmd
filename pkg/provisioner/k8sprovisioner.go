36193f52c6b0c392dc0820ca0e570b38945890e3
Alexamakans <fougner.alexander@gmail.com>
2022-05-06 11:52:41 +0200

    Mount cert volume to both init and app container

--- pkg/provisioner/k8sprovisioner.go
+++ pkg/provisioner/k8sprovisioner.go
@@ -115,17 +115,17 @@ func (p k8sProvisioner) newWorkerPod(args WorkerArgs) v1.Pod {
 			Name:      repoVolumeName,
 			MountPath: repoVolumeMountPath,
 		},
+		{
+			Name:      certVolumeName,
+			ReadOnly:  true,
+			MountPath: certVolumeMountPath,
+		},
 	}
+
 	gitVolumeMounts := append(volumeMounts, v1.VolumeMount{
 		Name:      sshVolumeName,
 		ReadOnly:  true,
 		MountPath: "/root/.ssh",
-	})
-
-	gitVolumeMounts = append(gitVolumeMounts, v1.VolumeMount{
-		Name:      certVolumeName,
-		ReadOnly:  true,
-		MountPath: certVolumeMountPath,
 	})
 
 	gitArgs := []string{"git", "clone", args.GitCloneURL, "--single-branch"}
