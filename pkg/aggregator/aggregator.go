079c0dc512de9db2cbbaa145977d51585c5ac479
Alexamakans <fougner.alexander@gmail.com>
2022-05-17 11:52:51 +0200

    Removed comment for relay function, as it is short and fairly self-explanatory

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -9,11 +9,6 @@ type Aggregator interface {
 	Serve(ctx context.Context) error
 }
 
-// relay takes a source and relays values from it by passing them to the
-// provided function.
-//
-// If the provided function returns an error, relaying will stop and the error
-// will be returned as-is.
 func relay[T any](src Source[T], relayFunc func(v T) error) error {
 	dst := make(chan T)
 	go func() {
