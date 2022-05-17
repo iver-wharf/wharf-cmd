0e1b9669cf332ad331814b0a909eb8b36799299f
kalle (jag) <kalle.fagerberg@iver.se>
2022-05-17 08:20:59 +0200

    Update pkg/varsub/varsub.go
    Co-authored-by: Alexamakans <79503481+Alexamakans@users.noreply.github.com>

--- pkg/varsub/varsub.go
+++ pkg/varsub/varsub.go
@@ -58,7 +58,7 @@ func substituteRec(value string, source Source, usedParams []string) (any, error
 			continue
 		}
 
-		var matchVal = v.Value
+		matchVal := v.Value
 		if str, ok := matchVal.(string); ok && strings.Contains(str, "${") {
 			var err error
 			matchVal, err = substituteRec(str, source, append(usedParams, match.Name))
