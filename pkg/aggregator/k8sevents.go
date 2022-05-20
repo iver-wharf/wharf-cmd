0ab10778d9cded175f3a3aca39a4336fa0591edf
Alexamakans <79503481+Alexamakans@users.noreply.github.com>
2022-05-20 09:21:32 +0200

    Fixed build getting stuck on bad config and/or wharf-ci file (#163)
    * save progress
    
    * save progress
    
    * Changed to detect if pod errored using init container and container statuses Waiting Reason
    
    * make stuff work
    
    * refactored aggregator
    
    * store progress
    
    * Fixed errors and cleaned up
    
    * Removed comment for relay function, as it is short and fairly self-explanatory
    
    * store
    
    * Refactor - Applied suggestions from code review
    
    * Removed unused ctx parameter
    
    * fixed calling of getEvents
    
    * Readded pinging of workerserver
    
    * Apply suggestions from code review
    
    Co-authored-by: kalle (jag) <kalle.fagerberg@iver.se>
    
    * Applied suggestions from code review
    
    * Updated CHANGELOG.md
    
    * Update pkg/aggregator/k8sevents.go
    
    Co-authored-by: kalle (jag) <kalle.fagerberg@iver.se>
    
    Co-authored-by: kalle (jag) <kalle.fagerberg@iver.se>

---
+++ pkg/aggregator/k8sevents.go
@@ -0,0 +1,85 @@
+// Copyright 2014 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aggregator
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/duration"
+)
+
+// Modified copy from:
+//   https://github.com/kubernetes/kubernetes/blob/b6a0718858876bbf8cedaeeb47e6de7e650a6c5b/pkg/kubectl/describe/versioned/describe.go#L3247
+func describeEvents(el *v1.EventList) io.ReadCloser {
+	var b bytes.Buffer
+	if len(el.Items) == 0 {
+		b.WriteString("<none>\n")
+		return io.NopCloser(&b)
+	}
+
+	tw := tabwriter.NewWriter(&b, 1, 4, 2, ' ', 0)
+	fmt.Fprintln(tw, "Type\tReason\tAge\tFrom\tMessage")
+	fmt.Fprintln(tw, "----\t------\t---\t----\t-------")
+	for _, e := range el.Items {
+		var interval string
+		if e.Count > 1 {
+			interval = fmt.Sprintf("%s (x%d over %s)", translateTimestampSince(e.LastTimestamp), e.Count, translateTimestampSince(e.FirstTimestamp))
+		} else {
+			interval = translateTimestampSince(e.FirstTimestamp)
+		}
+		fmt.Fprintf(tw, "%v\t%v\t%s\t%v\t%v\n",
+			e.Type,
+			e.Reason,
+			interval,
+			formatEventSource(e.Source),
+			strings.TrimSpace(e.Message),
+		)
+	}
+	tw.Flush()
+
+	return io.NopCloser(&b)
+}
+
+// translateTimestampSince returns the elapsed time since timestamp in
+// human-readable approximation.
+//
+// Verbatim copy from:
+//   https://github.com/kubernetes/kubernetes/blob/b6a0718858876bbf8cedaeeb47e6de7e650a6c5b/pkg/kubectl/describe/versioned/describe.go#L4299
+func translateTimestampSince(timestamp metav1.Time) string {
+	if timestamp.IsZero() {
+		return "<unknown>"
+	}
+
+	return duration.HumanDuration(time.Since(timestamp.Time))
+}
+
+// formatEventSource formats EventSource as a comma separated string excluding Host when empty
+//
+// Verbatim copy from:
+//   https://github.com/kubernetes/kubernetes/blob/b6a0718858876bbf8cedaeeb47e6de7e650a6c5b/pkg/kubectl/describe/versioned/describe.go#L4308
+func formatEventSource(es v1.EventSource) string {
+	EventSourceString := []string{es.Component}
+	if len(es.Host) > 0 {
+		EventSourceString = append(EventSourceString, es.Host)
+	}
+	return strings.Join(EventSourceString, ", ")
+}
---
+++ pkg/aggregator/piper_statusevents.go
@@ -0,0 +1,117 @@
+package aggregator
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/iver-wharf/wharf-api-client-go/v2/pkg/model/request"
+	"github.com/iver-wharf/wharf-api-client-go/v2/pkg/wharfapi"
+	v1 "github.com/iver-wharf/wharf-cmd/api/workerapi/v1"
+	"github.com/iver-wharf/wharf-cmd/pkg/workerapi/workerclient"
+)
+
+type statusEventsPiper struct {
+	wharfapi   wharfapi.Client
+	worker     workerclient.Client
+	in         v1.Worker_StreamStatusEventsClient
+	prevStatus request.BuildStatus
+}
+
+func newStatusEventsPiper(ctx context.Context, wharfapi wharfapi.Client, worker workerclient.Client) (statusEventsPiper, error) {
+	in, err := worker.StreamStatusEvents(ctx, &workerclient.StatusEventsRequest{})
+	if err != nil {
+		return statusEventsPiper{}, fmt.Errorf("open status events stream from wharf-cmd-worker: %w", err)
+	}
+	return statusEventsPiper{
+		wharfapi:   wharfapi,
+		worker:     worker,
+		in:         in,
+		prevStatus: request.BuildScheduling,
+	}, nil
+}
+
+func (p statusEventsPiper) PipeMessage() error {
+	status, err := p.readStatus()
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			p.ensureStatusCompletedOrFailed()
+		}
+		return fmt.Errorf("read status: %w", err)
+	}
+
+	transformedStatus, err := p.transformStatus(status.Status)
+	if err != nil {
+		return fmt.Errorf("transform status: %w", err)
+	}
+
+	if p.shouldSkip(transformedStatus) {
+		return nil
+	}
+
+	if err := p.writeStatus(transformedStatus); err != nil {
+		return fmt.Errorf("write status: %w", err)
+	}
+
+	return nil
+}
+
+// Close closes all active streams.
+func (p statusEventsPiper) Close() error {
+	if err := p.in.CloseSend(); err != nil {
+		log.Error().
+			WithError(err).
+			Message("Failed closing status events stream from worker.")
+	}
+	return nil
+}
+
+func (p statusEventsPiper) readStatus() (*workerclient.StatusEvent, error) {
+	if p.in == nil {
+		return nil, errors.New("input stream is nil")
+	}
+	status, err := p.in.Recv()
+	if err != nil {
+		return nil, err
+	}
+	return status, nil
+}
+
+func (p statusEventsPiper) ensureStatusCompletedOrFailed() error {
+	if p.prevStatus != request.BuildCompleted && p.prevStatus != request.BuildFailed {
+		finalStatus, _ := p.transformStatus(v1.StatusFailed)
+		if err := p.writeStatus(finalStatus); err != nil {
+			return fmt.Errorf("write status: %w", err)
+		}
+	}
+	return nil
+}
+
+func (p statusEventsPiper) shouldSkip(status request.BuildStatus) bool {
+	return status == p.prevStatus
+}
+
+func (p statusEventsPiper) transformStatus(status v1.Status) (request.BuildStatus, error) {
+	switch status {
+	case v1.StatusPending, v1.StatusScheduling:
+		return request.BuildScheduling, nil
+	case v1.StatusRunning, v1.StatusInitializing:
+		return request.BuildRunning, nil
+	case v1.StatusSuccess:
+		return request.BuildCompleted, nil
+	case v1.StatusCancelled, v1.StatusFailed:
+		return request.BuildFailed, nil
+	default:
+		return "", fmt.Errorf("unsupported status %q", status)
+	}
+}
+
+func (p statusEventsPiper) writeStatus(status request.BuildStatus) error {
+	statusUpdate := request.LogOrStatusUpdate{Status: status}
+	_, err := p.wharfapi.UpdateBuildStatus(p.worker.BuildID(), statusUpdate)
+	if err != nil {
+		p.prevStatus = status
+	}
+	return err
+}
