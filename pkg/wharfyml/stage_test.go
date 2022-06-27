d1ac73dcdec7e3e442b9bbb96b93f9414dfea69e
Alexamakans <fougner.alexander@gmail.com>
2022-06-27 12:57:16 +0200

    Added tests for new method Stage.ShouldSkip

--- pkg/wharfyml/stage_test.go
+++ pkg/wharfyml/stage_test.go
@@ -28,3 +28,68 @@ func TestVisitStage_Name(t *testing.T) {
 	}
 	assert.Equal(t, "myStage", stage.Name)
 }
+
+func TestShouldRun(t *testing.T) {
+	testCases := []struct {
+		name                      string
+		s                         Stage
+		wantSkip                  bool
+		anyPreviousStageHasFailed bool
+	}{
+		{
+			name:                      "'always' should run on fail",
+			s:                         Stage{RunsIf: StageRunsIfAlways},
+			wantSkip:                  false,
+			anyPreviousStageHasFailed: true,
+		},
+		{
+			name:                      "'always' should run on success",
+			s:                         Stage{RunsIf: StageRunsIfAlways},
+			wantSkip:                  false,
+			anyPreviousStageHasFailed: false,
+		},
+		{
+			name:                      "'failed' should run on fail",
+			s:                         Stage{RunsIf: StageRunsIfFail},
+			wantSkip:                  false,
+			anyPreviousStageHasFailed: true,
+		},
+		{
+			name:                      "'failed' should not run on success",
+			s:                         Stage{RunsIf: StageRunsIfFail},
+			wantSkip:                  true,
+			anyPreviousStageHasFailed: false,
+		},
+		{
+			name:                      "'success' should run on success",
+			s:                         Stage{RunsIf: StageRunsIfSuccess},
+			wantSkip:                  false,
+			anyPreviousStageHasFailed: false,
+		},
+		{
+			name:                      "'success' should not run on fail",
+			s:                         Stage{RunsIf: StageRunsIfSuccess},
+			wantSkip:                  true,
+			anyPreviousStageHasFailed: true,
+		},
+		{
+			name:                      "empty should run on success - default value",
+			s:                         Stage{},
+			wantSkip:                  false,
+			anyPreviousStageHasFailed: false,
+		},
+		{
+			name:                      "empty should not run on fail - default value",
+			s:                         Stage{},
+			wantSkip:                  true,
+			anyPreviousStageHasFailed: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.s.ShouldSkip(tc.anyPreviousStageHasFailed)
+			assert.Equal(t, tc.wantSkip, got)
+		})
+	}
+}
